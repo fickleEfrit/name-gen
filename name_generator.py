0681915cdab9184d3d264a47b823622a86e69920
jonah <powerhungry7@gmail.com>
2017-09-28 16:57:56 -0400

    Added writing to file and basic CLI

--- name_generator.py
+++ name_generator.py
@@ -1,4 +1,5 @@
 import random
+import sys
 """
 Name Generator made by Jonah Fritz
 Uses 2 files, names.txt and surnames.txt. Both files should contain only names separated by spaces.
@@ -19,6 +20,9 @@ def read_text(filename):
                 appended_text.append(word)
     return appended_text
 
+"""
+Chooses a random name from the text files - possibly use sys args to have non-static file names?
+"""
 def create_random_name():
     names = read_text("names.txt")
     surnames = read_text("surnames.txt")
@@ -26,7 +30,41 @@ def create_random_name():
     random_surname = surnames[random.randint(0, len(surnames) - 1)]
     return random_name + " " + random_surname
 
+def add_name(name):
+    with open("names.txt", "a") as file:
+        file.write(" " + name + " ")
+
+def add_surname(surname):
+    with open("surnames.txt", "a") as file:
+        file.write(" " + surname + " ")
+
+command_list = ("stop", "add_name", "add_surname", "help", "random_name")
 def main():
-    print(create_random_name())
+    command = ""
+    while(command != "stop"):
+        print("Enter a command, stop to exit")
+        command = input()
+        if command not in command_list:
+            print("Unrecognized command, type help for command list.")
+        #print list of commands
+        elif(command == "help"):
+            help_response = "Commands: "
+            for str in command_list:
+                help_response += str + " "
+            print(help_response)
+        #add a name
+        elif(command == "add_name"):
+            name_to_add = input("Enter name to add: ")
+            add_name(name_to_add)
+            print("Added " + name_to_add + " to names.txt")
+        #add a surname
+        elif(command == "add_surname"):
+            surname_to_add = input("Enter surname to add: ")
+            add_surname(surname_to_add)
+            print("Added " + surname_to_add + " to names.txt")
+        #generate a random name
+        elif(command == "random_name"):
+            print(create_random_name())
+    exit()
 
 main()
